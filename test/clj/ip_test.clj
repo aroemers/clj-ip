3111a90715e1a34f22e934553aaf4507af4c4d88
Arnout Roemers <github@functionalbytes.nl>
2015-09-30 09:26:52 +0200

    Add tests

--- test/clj/ip_test.clj
+++ test/clj/ip_test.clj
@@ -1,16 +1,45 @@
-(ns cljip.core-test
-  (:require [clojure.test :refer :all]
-            [clj.ip :refer :all]))
+(ns clj.ip-test
+  (:refer-clojure :exclude (compile))
+  (:require [clj.ip :refer :all]
+            [clojure.set :refer (difference)]
+            [clojure.test :refer :all]))
 
-(def allowed-ranges
-  (mapv compile
-        [;; Test server network
-         "91.220.189.12/30"
-         "91.220.189.16/32"
-         ;; Live network
-         "94.236.54.30/32"
-         "194.247.166.0/23"
-         "195.28.166.0/24" ]))
 
-(def in-ranges?
-  (apply some-fn allowed-ranges))
+;;; Helper definitions.
+
+(def ^:dynamic *rand-tests* 100000)
+
+(defn test-rand-ips [r]
+  (-> (for [n (range *rand-tests*)
+            :let [i (str (rand-int 256) "." (rand-int 256) "." (rand-int 256) "." (rand-int 256))]
+            :when (has-ip? r i)]
+        i)
+      (set)))
+
+
+;;; Tests
+
+(deftest uncompiled-test
+  (are [range ip in?] (= (has-ip? range ip) in?)
+    "1.1.1.12/30" "1.1.1.11" false
+    "1.1.1.12/30" "1.1.1.12" true
+    "1.1.1.12/30" "1.1.1.13" true
+    "1.1.1.12/30" "1.1.1.14" true
+    "1.1.1.12/30" "1.1.1.15" true
+    "1.1.1.12/30" "1.1.1.16" false)
+  (is (empty? (difference (test-rand-ips "1.1.1.12/30")
+                          #{"1.1.1.12" "1.1.1.13" "1.1.1.14" "1.1.1.15"}))))
+
+
+
+(deftest compiled-test
+  (let [compiled (compile "1.1.1.12/30")]
+    (are [range ip in?] (= (has-ip? range ip) in?)
+      compiled "1.1.1.11" false
+      compiled "1.1.1.12" true
+      compiled "1.1.1.13" true
+      compiled "1.1.1.14" true
+      compiled "1.1.1.15" true
+      compiled "1.1.1.16" false)
+    (is (empty? (difference (test-rand-ips compiled)
+                            #{"1.1.1.12" "1.1.1.13" "1.1.1.14" "1.1.1.15"})))))
