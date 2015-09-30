8876cd2666fc076c6814e48441808195ecc0a66b
Arnout Roemers <github@functionalbytes.nl>
2015-09-30 09:26:21 +0200

    Set version to 0.9.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject cljip "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clj-ip "0.9.0"
+  :description "A Clojure library for testing whether an IP address is in a subnet"
+  :url "https://github.com/aroemers/clj-ip"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]])
